0781293444cab32c0092a64f9a815c11a52a61ac
gnivler <gnivil@gmail.com>
2022-10-04 22:32:36 -0600

    rename project

--- Extensions.cs
+++ Extensions.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
+using HarmonyLib;
 using TaleWorlds.CampaignSystem.Roster;
 using TaleWorlds.Core;
 using TaleWorlds.LinQuick;
 
-namespace UniqueTroopsGoneWild
+namespace GloriousTroops
 {
     internal static class Extensions
     {
+        private static readonly AccessTools.FieldRef<TroopRoster, List<TroopRosterElement>> Elements = AccessTools.FieldRefAccess<TroopRoster, List<TroopRosterElement>>("_troopRosterElements");
+
         internal static int CountMounted(this TroopRoster troopRoster)
         {
             return troopRoster.GetTroopRoster().WhereQ(t => !t.Character.FirstBattleEquipment[10].IsEmpty).SumQ(t => t.Number);
@@ -16,14 +20,14 @@ namespace UniqueTroopsGoneWild
             return element.Item?.Tierf ?? element.ItemValue;
         }
 
-        internal static TroopRosterElement? AllSimilar(this TroopRosterElement element, TroopRoster roster)
+        internal static TroopRosterElement? GetNewAggregateTroopRosterElement(this TroopRosterElement element, TroopRoster roster)
         {
             if (element.Character is null)
-            {
                 return null;
-            }
-            var sumAlive = roster.GetTroopRoster().WhereQ(e => e.Character.Name.Equals(element.Character.Name)).SumQ(t => t.Number);
-            var sumWounded = roster.GetTroopRoster().WhereQ(e => e.Character.Name.Equals(element.Character.Name)).SumQ(t => t.WoundedNumber);
+            if (element.Character.IsHero || element.Character.OriginalCharacter is null)
+                return element;
+            var sumAlive = Elements(roster).WhereQ(e => e.Character.Name.Equals(element.Character.Name)).SumQ(t => t.Number);
+            var sumWounded = Elements(roster).WhereQ(e => e.Character.Name.Equals(element.Character.Name)).SumQ(t => t.WoundedNumber);
             var result = new TroopRosterElement(element.Character)
             {
                 Number = sumAlive,
